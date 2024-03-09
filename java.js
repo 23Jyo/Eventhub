f06260a8e32b1c9891f0023a384765eb470ed1da
Jyothsnabey <jyothsnaabey2003@gmail.com>
2024-03-09 22:06:52 +0530

    comment

---
+++ java.js
@@ -0,0 +1,5 @@
+const nextPageButton = document.getElementById('nextPageButton');
+
+nextPageButton.addEventListener('click', function() {
+  window.location.href = 'nextpage.html'; // Change 'nextpage.html' to the desired URL of the next page
+});
